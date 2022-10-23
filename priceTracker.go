e40864b5a856f663f66239a8487fb05963babd5b
Zealy <40661044+ljg-cqu@users.noreply.github.com>
2022-10-24 06:26:03 +0800

    Update priceTracker.go

--- priceTracker.go
+++ priceTracker.go
@@ -3,8 +3,11 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"github.com/ljg-cqu/biance/backoff"
+	"github.com/ljg-cqu/biance/logger"
+	"github.com/pkg/errors"
+
 	"io/ioutil"
-	"log"
 	"math/big"
 	"net/http"
 	"sync"
@@ -12,6 +15,7 @@ import (
 )
 
 type PriceTracker struct {
+	Logger    logger.Logger
 	Interval  time.Duration
 	PricesCh  chan Prices
 	WaitGroup *sync.WaitGroup
@@ -25,30 +29,42 @@ func (p *PriceTracker) Run(ctx context.Context) {
 		case <-ctx.Done():
 			return
 		case <-t.C:
-			prices, err := prices()
+			prices, err := p.prices(ctx)
+			p.Logger.ErrorOnError(err, "Failed to query prices")
 			if err != nil {
-				log.Printf("Failed to query prices:%v\n", err)
+				continue
 			}
 			p.PricesCh <- prices
 		}
 	}
 }
 
-func prices() (Prices, error) {
-	res, err := http.Get("https://api.binance.com/api/v3/ticker/price")
-	if err != nil {
-		return nil, err
-	}
-	defer res.Body.Close()
-	priceBytes, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		return nil, err
-	}
+func (p *PriceTracker) prices(ctx context.Context) (Prices, error) {
 	var prices Prices
-	err = json.Unmarshal(priceBytes, &prices)
+	err := backoff.RetryFnExponential100Times(p.Logger, ctx, time.Second, time.Second*10, func() (bool, error) {
+		res, err := http.Get("https://api.binance.com/api/v3/ticker/price")
+		if err != nil {
+			return true, errors.Wrapf(err, "failed to send request")
+		}
+
+		defer res.Body.Close()
+		priceBytes, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			return false, errors.Wrapf(err, "failed to read response body")
+		}
+
+		err = json.Unmarshal(priceBytes, &prices)
+		if err != nil {
+			return false, errors.Wrapf(err, "failed parse price")
+		}
+
+		return false, nil
+	})
+
 	if err != nil {
-		return nil, err
+		return nil, errors.WithStack(err)
 	}
+
 	now := time.Now()
 	for i, price := range prices {
 		priceFloat, _ := new(big.Float).SetString(price.Price)

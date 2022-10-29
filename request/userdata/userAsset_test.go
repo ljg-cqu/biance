99724efa2969db52eba6df219e1dfe47a94d0979
Zealy <40661044+ljg-cqu@users.noreply.github.com>
2022-10-29 14:12:10 +0800

    Update userAsset_test.go

--- request/userdata/userAsset_test.go
+++ request/userdata/userAsset_test.go
@@ -19,3 +19,15 @@ func TestGetUserAsset(t *testing.T) {
 		fmt.Printf("%+v\n", userAsset)
 	}
 }
+
+func TestGetUserAssetWithGivenAsset(t *testing.T) {
+	client := &http.Client{}
+	apiKey := "XesIIB7fra4VeCZQNovtnDgQI5QFt8073p3vWZBMIfbm3OLe0zl4A5YDs37961H0"
+	secretKey := "GDpqvNk4spXqKfEQom9nr852NDmmaCxKWpDTdtoGZxUW2uGsl37rX5iMu9JBhBam"
+	userAssets, err := getUserAsset(client, request.URLs[request.URLUserAsset], "BTC", apiKey, secretKey)
+	require.Nil(t, err)
+	fmt.Println(len(userAssets))
+	for _, userAsset := range userAssets {
+		fmt.Printf("%+v\n", userAsset)
+	}
+}

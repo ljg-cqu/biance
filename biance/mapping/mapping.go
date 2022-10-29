fb50940d7084ae7ae238ef1d82cd5096882a108e
Zealy <40661044+ljg-cqu@users.noreply.github.com>
2022-10-29 20:25:27 +0800

    Add files via upload

---
+++ biance/mapping/mapping.go
@@ -0,0 +1,96 @@
+package mapping
+
+import (
+	"github.com/ljg-cqu/biance/biance"
+	"github.com/ljg-cqu/biance/biance/price"
+	"github.com/pkg/errors"
+	"math/big"
+	"strings"
+)
+
+type Mapped struct {
+	Symbol    price.Symbol
+	TargetAmt *big.Float
+	BaseAmt   *big.Float
+	Price     *big.Float
+}
+
+type Pair struct {
+	Symbol  price.Symbol
+	BaseAmt *big.Float
+}
+
+var getPriceFn = price.GetPrice
+
+func MappingWithBUSDOrUSDT(client biance.Client, priceUrl string, isReverse bool, paris ...*Pair) ([]Mapped, error) {
+	var editPair = paris
+	var prices []price.Price
+	for i, pair := range editPair {
+		if !strings.Contains(string(pair.Symbol), "BUSD") && !strings.Contains(string(pair.Symbol), "USDT") {
+			return nil, errors.New("only BUSD or USDT supported")
+		}
+
+		var pricesOne []price.Price
+		var symbol = pair.Symbol
+		var err error
+		switch {
+		case strings.HasSuffix(string(pair.Symbol), "BUSD"):
+			pricesOne, err = getPriceFn(client, priceUrl, pair.Symbol)
+			if err != nil {
+				symbol = price.Symbol(strings.TrimSuffix(string(pair.Symbol), "BUSD") + "USDT")
+				pricesOne, err = getPriceFn(client, priceUrl, symbol)
+				if err != nil {
+					return nil, errors.Wrapf(err, "failed to get price for symbol %v", symbol)
+				}
+			}
+		case strings.HasSuffix(string(pair.Symbol), "USDT"):
+			pricesOne, err = getPriceFn(client, priceUrl, pair.Symbol)
+			if err != nil {
+				symbol = price.Symbol(strings.TrimSuffix(string(pair.Symbol), "USDT") + "BUSD")
+				pricesOne, err = getPriceFn(client, priceUrl, symbol)
+				if err != nil {
+					return nil, errors.Wrapf(err, "failed to get price for symbol %v", symbol)
+				}
+			}
+		}
+
+		editPair[i].Symbol = symbol
+		prices = append(prices, pricesOne[0])
+	}
+	return mapping(prices, isReverse, paris...), nil
+}
+
+func Mapping(client biance.Client, priceUrl string, isReverse bool, paris ...*Pair) ([]Mapped, error) {
+	var symbols []price.Symbol
+	for _, pair := range paris {
+		symbols = append(symbols, pair.Symbol)
+	}
+	prices, err := getPriceFn(client, priceUrl, symbols...)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get price")
+	}
+
+	return mapping(prices, isReverse, paris...), nil
+}
+
+func mapping(prices []price.Price, isReverse bool, pairs ...*Pair) []Mapped {
+	var pricesMap = make(map[price.Symbol]*big.Float)
+	for _, price := range prices {
+		pricesMap[price.Symbol] = price.Price
+	}
+
+	var mappeds []Mapped
+	for _, pair := range pairs {
+		price := pricesMap[pair.Symbol]
+
+		var amtMapped *big.Float
+		if isReverse {
+			amtMapped = new(big.Float).Quo(pair.BaseAmt, price)
+		} else {
+			amtMapped = new(big.Float).Mul(price, pair.BaseAmt)
+		}
+
+		mappeds = append(mappeds, Mapped{pair.Symbol, amtMapped, pair.BaseAmt, price})
+	}
+	return mappeds
+}
---
+++ biance/mapping/mapping_test.go
@@ -0,0 +1,67 @@
+package mapping
+
+import (
+	"fmt"
+	"github.com/ljg-cqu/biance/biance"
+	"github.com/ljg-cqu/biance/biance/price"
+	"github.com/stretchr/testify/require"
+	"math/big"
+	"net/http"
+	"strings"
+	"testing"
+)
+
+func TestMappingWithBUSDOrUSDT(t *testing.T) {
+	TokenValueRaw := "MDX:11.18 AERGO:4.02 OCEAN:3.38 PYR:3.21 BCH:2.49 COTI:2.47 WAVES:2.43 ALPINE:2.42 WTC:2.38 FLM:2.36 NKN:2.21 AMB:2.21 AXS:2.20 REEF:2.09 BTTC:2.08 ASR:2.08 GAL:2.06 CREAM:2.06 ETH:2.02"
+	tokenValuePairs := strings.Fields(TokenValueRaw)
+
+	var pairs []*Pair
+
+	for _, tokenValuePair := range tokenValuePairs {
+		tokenValue := strings.Split(tokenValuePair, ":")
+		amtToMap, _ := new(big.Float).SetString(tokenValue[1])
+		token := tokenValue[0]
+		symbol := price.Symbol(token + "BUSD")
+		pair := Pair{
+			Symbol:  symbol,
+			BaseAmt: amtToMap,
+		}
+		pairs = append(pairs, &pair)
+	}
+
+	client := &http.Client{}
+	mappeds, err := MappingWithBUSDOrUSDT(client, biance.URLs[biance.URLSymbolPrice], true, pairs...)
+	require.Nil(t, err)
+	for _, mapped := range mappeds {
+		fmt.Printf("%+v\n", mapped)
+	}
+}
+
+func TestMapping(t *testing.T) {
+	TokenValueRaw := "MDX:11.18 AERGO:4.02 OCEAN:3.38 PYR:3.21 BCH:2.49 COTI:2.47 WAVES:2.43 ALPINE:2.42 WTC:2.38 FLM:2.36 NKN:2.21 AMB:2.21 AXS:2.20 REEF:2.09 BTTC:2.08 ASR:2.08 GAL:2.06 CREAM:2.06 ETH:2.02"
+	tokenValuePairs := strings.Fields(TokenValueRaw)
+
+	var pairs []*Pair
+
+	for _, tokenValuePair := range tokenValuePairs {
+		tokenValue := strings.Split(tokenValuePair, ":")
+		amtToMap, _ := new(big.Float).SetString(tokenValue[1])
+		token := tokenValue[0]
+		symbol := price.Symbol(token + "BUSD")
+		if token == "WTC" {
+			symbol = price.Symbol(token + "USDT")
+		}
+		pair := Pair{
+			Symbol:  symbol,
+			BaseAmt: amtToMap,
+		}
+		pairs = append(pairs, &pair)
+	}
+
+	client := &http.Client{}
+	mappeds, err := Mapping(client, biance.URLs[biance.URLSymbolPrice], true, pairs...)
+	require.Nil(t, err)
+	for _, mapped := range mappeds {
+		fmt.Printf("%+v\n", mapped)
+	}
+}

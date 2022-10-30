6ba2bc936964f03c3957c9cbeb90b1a58f439653
Zealy <40661044+ljg-cqu@users.noreply.github.com>
2022-10-30 12:03:39 +0800

    Update mapping_test.go

--- biance/mapping/mapping_test.go
+++ biance/mapping/mapping_test.go
@@ -11,9 +11,8 @@ import (
 	"testing"
 )
 
-func TestMappingWithBUSDOrUSDT(t *testing.T) {
-	TokenValueRaw := "KLAY:8.72 NEO:2.76 XEM:2.64 BTTC:2.48 POLS:2.21 BAT:2 GALA:1.97 " +
-		"STORJ:1.95 LRC:1.92 BAL:1.9 API3:1.89 SKL:1.84"
+func TestMappingBUSD(t *testing.T) {
+	TokenValueRaw := "PORTO:200"
 	tokenValuePairs := strings.Fields(TokenValueRaw)
 
 	var pairs []*Pair
@@ -31,7 +30,7 @@ func TestMappingWithBUSDOrUSDT(t *testing.T) {
 	}
 
 	client := &http.Client{}
-	mappeds, err := MappingWithBUSDOrUSDT(client, biance.URLs[biance.URLSymbolPrice], true, pairs...)
+	mappeds, err := MappingBUSD(client, biance.URLs[biance.URLSymbolPrice], false, pairs...)
 	require.Nil(t, err)
 	for _, mapped := range mappeds {
 		fmt.Printf("%+v\n", mapped)
@@ -39,8 +38,7 @@ func TestMappingWithBUSDOrUSDT(t *testing.T) {
 }
 
 func TestMapping(t *testing.T) {
-	TokenValueRaw := "MDX:11.18 AERGO:4.02 OCEAN:3.38 PYR:3.21 BCH:2.49 COTI:2.47 WAVES:2.43 ALPINE:2.42 WTC:2.38 " +
-		"FLM:2.36 NKN:2.21 AMB:2.21 AXS:2.20 REEF:2.09 BTTC:2.08 ASR:2.08 GAL:2.06 CREAM:2.06 ETH:2.02"
+	TokenValueRaw := "EGLD:2.29832413"
 	tokenValuePairs := strings.Fields(TokenValueRaw)
 
 	var pairs []*Pair
@@ -61,7 +59,7 @@ func TestMapping(t *testing.T) {
 	}
 
 	client := &http.Client{}
-	mappeds, err := Mapping(client, biance.URLs[biance.URLSymbolPrice], true, pairs...)
+	mappeds, err := Mapping(client, biance.URLs[biance.URLSymbolPrice], false, pairs...)
 	require.Nil(t, err)
 	for _, mapped := range mappeds {
 		fmt.Printf("%+v\n", mapped)

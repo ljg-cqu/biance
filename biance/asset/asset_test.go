039336fa4c376ea02e7aafefef70505d62135710
Zealy <40661044+ljg-cqu@users.noreply.github.com>
2022-10-30 16:17:11 +0800

    Update asset_test.go

--- biance/asset/asset_test.go
+++ biance/asset/asset_test.go
@@ -4,19 +4,36 @@ import (
 	"fmt"
 	"github.com/ljg-cqu/biance/biance"
 	"github.com/stretchr/testify/require"
+	"math/big"
 	"net/http"
+	"strings"
 	"testing"
 )
 
-func TestGetAssetWithDolVal(t *testing.T) {
+func TestGetAssetWithUSDTOrBUSDFreeValue(t *testing.T) {
 	client := &http.Client{}
 	apiKey := ""
 	secretKey := ""
 	assetURL := biance.URLs[biance.URLUserAsset]
 	priceURL := biance.URLs[biance.URLSymbolPrice]
-	userAssets, err := GetAssetWithUSDTOrBUSD(client, assetURL, priceURL, "", apiKey, secretKey)
+	userAssets, err := GetAssetWithUSDTOrBUSDFreeValue(client, assetURL, priceURL, "", apiKey, secretKey)
 	require.Nil(t, err)
 	fmt.Println(len(userAssets))
+	principal, _ := new(big.Float).SetString("150")
+	var richTokens string
+	for _, userAsset := range userAssets {
+		if userAsset.FreeValue.Cmp(principal) == 1 {
+			richTokens += fmt.Sprintf("\"%v\",", userAsset.Token)
+		}
+	}
+	fmt.Println(richTokens)
+	for _, userAsset := range userAssets {
+		token := string(userAsset.Symbol)
+		token = strings.TrimSuffix(token, "USDT")
+		token = strings.TrimSuffix(token, "BUSD")
+		fmt.Printf("%v:%+v (%v)\n", token, userAsset.FreeValue, userAsset.Symbol)
+	}
+	fmt.Println("-----------------")
 	for _, userAsset := range userAssets {
 		fmt.Printf("%+v\n", userAsset)
 	}

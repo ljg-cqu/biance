32b9f38bdc64a766935756a5173fd54679c294ca
Zealy <40661044+ljg-cqu@users.noreply.github.com>
2022-10-31 22:30:32 +0800

    Add files via upload

---
+++ email/email.go
@@ -0,0 +1,53 @@
+package email
+
+import (
+	"context"
+	"crypto/tls"
+	"github.com/ljg-cqu/biance/logger"
+	"github.com/ljg-cqu/biance/utils/backoff"
+	"github.com/ljg-cqu/core/smtp"
+	"github.com/pkg/errors"
+	mail "github.com/xhit/go-simple-mail/v2"
+	"time"
+)
+
+func SendPNLReportWith126Mail(log logger.Logger, ctx context.Context, subject, content string) error {
+	email := mail.NewMSG()
+	email.SetFrom("Zealy <ljg_cqu@126.com>").
+		AddTo("ljg_cqu@126.com", "1025003548@qq.com").
+		SetSubject(subject)
+	email.SetBody(mail.TextPlain, content)
+	err := backoff.RetryFnExponential10Times(log, ctx, time.Second, time.Second*10, func() (bool, error) {
+		emailCli, err := smtp.NewEmailClient(smtp.NetEase126Mail, &tls.Config{InsecureSkipVerify: true}, "ljg_cqu@126.com", "XROTXFGWZUILANPB")
+		if err != nil {
+			return true, errors.Wrapf(err, "failed to create email client.")
+		}
+		err = emailCli.Send(email)
+		if err != nil {
+			return true, errors.Wrapf(err, "failed to send email")
+		}
+		return false, nil
+	})
+	return errors.WithStack(err)
+}
+
+func SendPNLReportWithQQMail(log logger.Logger, ctx context.Context, subject, content string) error {
+	email := mail.NewMSG()
+	email.SetFrom("Zealy <1025003548@qq.com>").
+		AddTo("ljg_cqu@126.com", "1025003548@qq.com").
+		SetSubject(subject)
+	email.SetBody(mail.TextPlain, content)
+
+	err := backoff.RetryFnExponential10Times(log, ctx, time.Second, time.Second*10, func() (bool, error) {
+		emailCli, err := smtp.NewEmailClient(smtp.QQMail, &tls.Config{InsecureSkipVerify: true}, "1025003548@qq.com", "ncoajiivbenpbfbh")
+		if err != nil {
+			return true, errors.Wrapf(err, "failed to create email client.")
+		}
+		err = emailCli.Send(email)
+		if err != nil {
+			return true, errors.Wrapf(err, "failed to send email")
+		}
+		return false, nil
+	})
+	return errors.WithStack(err)
+}

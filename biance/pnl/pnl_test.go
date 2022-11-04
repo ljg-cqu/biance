3ebbdf4616abebf963096d1c6e709c8273ee75f5
Zealy <40661044+ljg-cqu@users.noreply.github.com>
2022-11-04 18:10:17 +0800

    Create pnl_test.go

--- biance/pnl/pnl_test.go
+++ biance/pnl/pnl_test.go
@@ -36,40 +36,40 @@ func TestCheckFreePNLWithUSDTOrBUSD(t *testing.T) {
 		freePNLsFilter = append(freePNLsFilter, freePNL)
 	}
 
-	var considerableGianPNLs []FreePNL
-	var considerableLossPNLs []FreePNL
+	var gainPNLs []FreePNL
+	var lossPNLs []FreePNL
 	zeroGain, _ := new(big.Float).SetString("0")
 	zeroLoss, _ := new(big.Float).SetString("0")
 
-	one, _ := new(big.Float).SetString("1")
-	considerableLossPercent, _ := new(big.Float).SetString("0.03")
+	gailThreshold, _ := new(big.Float).SetString("0.05")
+	lossThreshold, _ := new(big.Float).SetString("0.10")
 	var totalGain = zeroGain
 	var totalLoss = zeroLoss
 
 	oneHundred, _ := new(big.Float).SetString("100")
 
 	for _, freePNL := range freePNLsFilter {
-		if freePNL.PNLValue.Cmp(one) == 1 {
-			considerableGianPNLs = append(considerableGianPNLs, freePNL)
+		if freePNL.PNLPercent.Cmp(gailThreshold) == 1 {
+			gainPNLs = append(gainPNLs, freePNL)
 			totalGain = new(big.Float).Add(totalGain, freePNL.PNLValue)
 			continue
 		}
-		if new(big.Float).Abs(freePNL.PNLPercent).Cmp(considerableLossPercent) == 1 {
-			considerableLossPNLs = append(considerableLossPNLs, freePNL)
+		if new(big.Float).Abs(freePNL.PNLPercent).Cmp(lossThreshold) == 1 {
+			lossPNLs = append(lossPNLs, freePNL)
 			totalLoss = new(big.Float).Add(totalLoss, freePNL.PNLValue)
 		}
 	}
 
 	var gainInfoStr = fmt.Sprintf("\n++++++++++++++++++++tokens:%v profit:%v++++++++++++++++++++\n",
-		len(considerableGianPNLs), totalGain)
-	for _, gainPNL := range considerableGianPNLs {
+		len(gainPNLs), totalGain)
+	for _, gainPNL := range gainPNLs {
 		gainInfoStr += fmt.Sprintf("%v %v => %v @%v%%\n", gainPNL.Symbol, gainPNL.PNLAmountConvertable,
 			gainPNL.PNLValue, new(big.Float).Mul(oneHundred, gainPNL.PNLPercent))
 	}
 
 	var lossInfoStr = fmt.Sprintf("--------------------tokens:%v loss:%v--------------------\n",
-		len(considerableLossPNLs), totalLoss)
-	for _, lossPNL := range considerableLossPNLs {
+		len(lossPNLs), totalLoss)
+	for _, lossPNL := range lossPNLs {
 		lossInfoStr += fmt.Sprintf("%v %v @%v%%\n", lossPNL.Symbol,
 			lossPNL.PNLValue, new(big.Float).Mul(oneHundred, lossPNL.PNLPercent))
 	}

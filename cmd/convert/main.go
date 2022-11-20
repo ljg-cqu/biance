8ab8dc4f200414c0366a29a3145c5c0c1efe45ba
ljg-cqu <ljg_cqu@126.com>
2022-11-20 23:24:05 +0800

    Add main.go

--- cmd/convert/main.go
+++ cmd/convert/main.go
@@ -9,7 +9,9 @@ import (
 	"github.com/ljg-cqu/biance/logger"
 	"math/big"
 	"net/http"
+	"os"
 	"strings"
+	"time"
 )
 
 func main() {
@@ -39,71 +41,93 @@ func main() {
 	client := &http.Client{}
 	assetURL := biance.URLs[biance.URLUserAsset]
 	priceURL := biance.URLs[biance.URLSymbolPrice]
-	freePNLs, err := pnl.CheckFreePNLWithUSDTOrBUSD(client, assetURL, priceURL, "", *apiKey, *secretKey)
-	if err != nil {
-		myLogger.ErrorOnError(err, "failed to check pnl")
-		return
-	}
 
-	var freePNLsFilter pnl.FreePNLs
-
-	var tokenFilerMap = map[asset.Token]string{
-		"NBS":  "",
-		"USDT": "",
-		"BUSD": "",
-		"VIDT": "",
-	}
-
-	for _, freePNL := range freePNLs {
-		_, ok := tokenFilerMap[freePNL.Token]
-		if ok {
+	for {
+		freePNLs, err := pnl.CheckFreePNLWithUSDTOrBUSD(client, assetURL, priceURL, "", *apiKey, *secretKey)
+		if err != nil {
+			myLogger.ErrorOnError(err, "failed to check pnl")
 			continue
 		}
-		freePNLsFilter = append(freePNLsFilter, freePNL)
-	}
 
-	var gainPNLs []pnl.FreePNL
-	var lossPNLs []pnl.FreePNL
-	zeroGain, _ := new(big.Float).SetString("0")
-	zeroLoss, _ := new(big.Float).SetString("0")
+		var freePNLsFilter pnl.FreePNLs
 
-	gailThreshold, _ := new(big.Float).SetString("0.05")
-	lossThreshold, _ := new(big.Float).SetString("0.05")
-	var totalGain = zeroGain
-	var totalLoss = zeroLoss
+		var tokenFilerMap = map[asset.Token]string{
+			"NBS":  "",
+			"USDT": "",
+			"BUSD": "",
+			"VIDT": "",
+		}
 
-	oneHundred, _ := new(big.Float).SetString("100")
-	zero, _ := new(big.Float).SetString("0")
-	for _, freePNL := range freePNLsFilter {
-		switch freePNL.PNLPercent.Cmp(zero) {
-		case 1:
-			if freePNL.PNLPercent.Cmp(gailThreshold) == 1 {
-				gainPNLs = append(gainPNLs, freePNL)
-				totalGain = new(big.Float).Add(totalGain, freePNL.PNLValue)
+		for _, freePNL := range freePNLs {
+			_, ok := tokenFilerMap[freePNL.Token]
+			if ok {
+				continue
 			}
-		case 0:
-		case -1:
-			if new(big.Float).Abs(freePNL.PNLPercent).Cmp(lossThreshold) == 1 {
-				lossPNLs = append(lossPNLs, freePNL)
-				totalLoss = new(big.Float).Add(totalLoss, freePNL.PNLValue)
+			freePNLsFilter = append(freePNLsFilter, freePNL)
+		}
+
+		var gainPNLs []pnl.FreePNL
+		var lossPNLs []pnl.FreePNL
+		zeroGain, _ := new(big.Float).SetString("0")
+		zeroLoss, _ := new(big.Float).SetString("0")
+
+		// TODO: config it
+		gailThreshold, _ := new(big.Float).SetString("0.05")
+		lossThreshold, _ := new(big.Float).SetString("0.05")
+		var totalGain = zeroGain
+		var totalLoss = zeroLoss
+
+		oneHundred, _ := new(big.Float).SetString("100")
+		zero, _ := new(big.Float).SetString("0")
+		for _, freePNL := range freePNLsFilter {
+			switch freePNL.PNLPercent.Cmp(zero) {
+			case 1:
+				if freePNL.PNLPercent.Cmp(gailThreshold) == 1 {
+					gainPNLs = append(gainPNLs, freePNL)
+					totalGain = new(big.Float).Add(totalGain, freePNL.PNLValue)
+				}
+			case 0:
+			case -1:
+				if new(big.Float).Abs(freePNL.PNLPercent).Cmp(lossThreshold) == 1 {
+					lossPNLs = append(lossPNLs, freePNL)
+					totalLoss = new(big.Float).Add(totalLoss, freePNL.PNLValue)
+				}
 			}
 		}
-	}
 
-	var gainInfoStr = fmt.Sprintf("\n++++++++++++++++++++tokens:%v profit:%v++++++++++++++++++++\n",
-		len(gainPNLs), totalGain)
-	for i, gainPNL := range gainPNLs {
-		gainInfoStr += fmt.Sprintf("(%v) %v %v => %v @%v%%\n", i, gainPNL.Symbol, gainPNL.PNLAmountConvertable,
-			gainPNL.PNLValue, new(big.Float).Mul(oneHundred, gainPNL.PNLPercent))
-	}
+		var gainInfoStr = fmt.Sprintf("\n++++++++++++++++++++tokens:%v profit:%v++++++++++++++++++++\n",
+			len(gainPNLs), totalGain)
+		for i, gainPNL := range gainPNLs {
+			gainInfoStr += fmt.Sprintf("(%v) %v %v => %v @%v%%\n", i, gainPNL.Symbol, gainPNL.PNLAmountConvertable,
+				gainPNL.PNLValue, new(big.Float).Mul(oneHundred, gainPNL.PNLPercent))
+		}
 
-	var lossInfoStr = fmt.Sprintf("--------------------tokens:%v loss:%v--------------------\n",
-		len(lossPNLs), totalLoss)
-	for i, lossPNL := range lossPNLs {
-		lossInfoStr += fmt.Sprintf("(%v) %v %v @%v%%\n", i, lossPNL.Symbol,
-			lossPNL.PNLValue, new(big.Float).Mul(oneHundred, lossPNL.PNLPercent))
-	}
+		var lossInfoStr = fmt.Sprintf("--------------------tokens:%v loss:%v--------------------\n",
+			len(lossPNLs), totalLoss)
+		for i, lossPNL := range lossPNLs {
+			lossInfoStr += fmt.Sprintf("(%v) %v %v @%v%%\n", i, lossPNL.Symbol,
+				lossPNL.PNLValue, new(big.Float).Mul(oneHundred, lossPNL.PNLPercent))
+		}
 
-	fmt.Println(gainInfoStr)
-	fmt.Println(lossInfoStr)
+		if *reportGain {
+			gainf, err := os.OpenFile("gain.txt", os.O_TRUNC|os.O_CREATE|os.O_WRONLY, 0644)
+			myLogger.ErrorOnError(err, "failed to open file")
+
+			_, err = gainf.WriteString(gainInfoStr)
+			myLogger.ErrorOnError(err, "failed to write string")
+
+			fmt.Println(gainInfoStr)
+		}
+
+		if *reportLoss {
+			lossf, err := os.OpenFile("loss.txt", os.O_TRUNC|os.O_CREATE|os.O_WRONLY, 0644)
+			myLogger.ErrorOnError(err, "failed to open file")
+
+			_, err = lossf.WriteString(lossInfoStr)
+			myLogger.ErrorOnError(err, "failed to write string")
+			fmt.Println(lossInfoStr)
+		}
+
+		time.Sleep(time.Second * 5)
+	}
 }
